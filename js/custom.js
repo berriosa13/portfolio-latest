0d816dfc21db4c4b73364799f961a81ce39cadb0
berriosa13 <berriosa13@gmail.com>
2023-04-09 18:27:34 -0400

    Added scroll-to-top button, changed brand icon to name

--- js/custom.js
+++ js/custom.js
@@ -58,3 +58,13 @@ chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
 });
   
+// function for smooth scroll to top
+const toTop = document.querySelector(".arrow-up");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
