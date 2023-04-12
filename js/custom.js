dc6f7db89f89c59c14e3b6fe72fe9dbb8272f432
berriosa13 <berriosa13@gmail.com>
2023-04-12 15:58:44 -0400

    Swtiched to dark mode as default color theme which the option to click back to light-mode

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,8 @@
   "use strict";
 
     // COLOR MODE
-    $('.color-mode').click(function(){
-        $('.color-mode-icon').toggleClass('active')
-        $('body').toggleClass('dark-mode')
+    $('.dark-mode').click(function(){
+        $('body').toggleClass('light-mode')
     })
 
     // HEADER
